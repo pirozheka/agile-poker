33e08786d2caff54fbe17a210ea9945b0d4ba262
Jay <pirozheka@gmail.com>
2024-04-07 21:13:09 +0500

    darkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
-import { Label } from "@/components/ui/label"
-import { Switch } from "@/components/ui/switch"
-
-
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    console.log('fjf')
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+
   return (
     <header> 
         <nav className='flex justify-between items-center'>
           <Link href="/">
-            <Image 
+            <img
               src="/icons/logo.png" 
               width={90}
               height={90}
@@ -18,7 +34,7 @@ const Header = () => {
             />
           </Link>
           <div className='flex justify-between items-center gap-3'>
-            <Switch id="dark-mode" />
+            <Switch className="dark:bg-white" id="dark-mode"  checked={isDarkMode} onClick={toggleDarkMode}/>
             <Label htmlFor="dark-mode">Темная тема</Label>
           </div>
         </nav>
