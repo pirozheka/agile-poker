0c76b958920a8d70b8ceaaf87b46423a0e8b198f
Никита Холодилов <121629961+Nikitalum@users.noreply.github.com>
2024-04-13 17:58:29 +0900

    Добавлены анимации карт, рука, карта спиной

--- src/components/about-cards/AboutCards.jsx
+++ src/components/about-cards/AboutCards.jsx
@@ -4,12 +4,14 @@ import './about-cards.css';
 const AbouCards = () => {
     return (
         <div className='relative'>
+            <div class="im7_0"><img src="/img/card_back.svg" alt=""></img></div>
             <div className="im7_1"><img src="/img/карта_команда3-1.svg" alt=""></img></div>
             <div className="im7_2"><img src="/img/карта_команда3-2.svg" alt=""></img></div>
             <div className="im7_3"><img src="/img/карта_команда3-3.svg" alt=""></img></div>
             <div className="im7_4"><img src="/img/карта_команда3-4.svg" alt=""></img></div>
             <div className="im7_5"><img src="/img/карта_команда3-5.svg" alt=""></img></div>
             <div className="im7_6"><img src="/img/карта_команда3-6.svg" alt=""></img></div>
+            <div class="im7_7"><img src="/img/vectorpaint_01.svg" alt=""></img></div>
         </div>
     );
 };
