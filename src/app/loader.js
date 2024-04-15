d53f101e3ee0cb97ad5f23ba9c294cbd5e399c37
Jay <you@example.com>
2024-04-15 09:22:25 +0500

    добавлен лоадер

---
+++ src/app/loader.js
@@ -0,0 +1,8 @@
+
+const Loader = () => (
+  <div className="gif-loader container flex justify-center items-center h-screen w-screen">
+    <img src="/img/loader.gif" alt="Loading..." style={{ width: '250px' }}/>
+  </div>
+);
+
+export default Loader;
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,38 @@
-
+'use client'
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import HeroContainer from "@/components/HeroContainer";
 import Submenu from "@/components/Submenu";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
+import React, { useState, useEffect } from 'react';
+import Loader from "./loader";
+
+
 
 export default function Home() {
-  return (
-    <main className="flex flex-col justify-between mx-auto overflow-hidden dark:bg-darkThemeBg">
-      <Header/>
-      <Hero />
-      <Submenu />
-      <HeroContainer />
-      <About />
-      <Footer />
-    </main>
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Логика загрузки данных или других действий
+    // Здесь можно использовать, например, setTimeout для имитации загрузки
+    setTimeout(() => {
+      setLoading(false); // Устанавливаем loading в false, когда загрузка завершена
+    }, 2000); // Имитация загрузки в течение 2 секунд
+  }, []);
+
+  return (
+    <div>
+      {loading ? <Loader /> : (
+      <main className="flex flex-col justify-between mx-auto overflow-hidden dark:bg-darkThemeBg">
+        <Header />
+        <Hero />
+        <Submenu />
+        <HeroContainer />
+        <About />
+        <Footer />
+      </main>
+      )}
+    </div>
   );
 }
