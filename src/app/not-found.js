e1c5e58be48b6caf9f6f54781b549796f41dea14
Nikitalum <chesterryos@gmail.com>
2024-04-17 00:32:06 +0900

    обновлена страница 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,18 @@
-'use client'
+import Link from 'next/link'
+import './notf.css'
 
 export default function NotFound() {
     return (
-        <div className="bg-white h-screen">
-            <div className="bg-white mt-auto">
-                <video className="w-full h-screen object-cover" autoPlay>
-                    <source src="/img/notfound.mp4" type="video/mp4" />
-                    <source src="/img/notfound.webm" type="video/webm" />
-                </video>
-            </div>
+        <div id="container object-center">
+            <div class="im_er_0"><img src="/img/веревка.svg" alt=""></img></div>
+            <div class="im_er_1"><img src="/img/карта_лицо_4.svg" alt=""></img></div>
+            <div class="im_er_2"><img src="/img/карта_лицо_0.svg" alt=""></img></div>
+            <div class="im_er_3"><img src="/img/прищепка_ор.svg" alt=""></img></div>
+            <div class="im_er_4"><img src="/img/прищепка_сер.svg" alt=""></img></div>
+            <div class="im_er_5"><img src="/img/прищепка_сер.svg" alt=""></img></div>
+            <div class="im_er_6"><img src="/img/прищепка_ор.svg" alt=""></img></div>
+            <div class="im_er_7"><img src="/img/карта_лицо_4.svg" alt=""></img></div>
+            <div class="submenu3_1 text-4xl"><h1>Ой-ой, страница не нашлась, <br />наверное в другой колоде</h1></div>
         </div>
     )
 }
