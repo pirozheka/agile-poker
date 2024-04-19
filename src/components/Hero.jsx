01149afbe32c2b6f2bdcc4ce6e6724b549eafa80
Jay <you@example.com>
2024-04-19 18:45:21 +0500

    Полностью адаптирован первый экран - моб, планшет

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,23 +20,23 @@ const Hero = () => {
 
 
   return (
-    <div className="mt-8 laptop:mt-28 flex justify-between relative container" >
+    <div className="mt-8 tablet:mt-14 laptop:mt-28 flex justify-between relative container" >
       <div>
-        <h2 className="text-3xl tablet:text-4xl laptop:text-[56px] tracking-wide leading-9 tablet:leading-[46px] laptop:leading-[68px] font-semibold text-mainText dark:text-darkThemeText">Покер планирование <br />для Agile-команд</h2>
-        <ul className="mt-2.5 laptop:mt-10 pl-4 laptop:pl-8">
-          <li className="text-lg laptop:text-[28px] tracking-wide laptop:mt-10 list-disc text-mainText dark:text-darkThemeText">Быстро и просто</li>
-          <li className="text-lg laptop:text-[28px] tracking-wide list-disc text-mainText dark:text-darkThemeText">Без разногласий в команде</li>
+        <h2 className="text-[28px] tablet:text-[38px] laptop:text-[56px] tracking-wide leading-9 tablet:leading-[46px] laptop:leading-[68px] font-semibold text-mainText dark:text-darkThemeText">Покер планирование <br />для Agile-команд</h2>
+        <ul className="mt-2.5 tablet:mt-8 laptop:mt-10 pl-4 laptop:pl-8">
+          <li className="text-lg laptop:text-[28px] font-light tablet:font-normal tracking-wide laptop:mt-10 list-disc text-mainText dark:text-darkThemeText">Быстро и просто</li>
+          <li className="text-lg laptop:text-[28px] font-light tablet:font-normal tracking-wide list-disc text-mainText dark:text-darkThemeText">Без разногласий в команде</li>
         </ul>
 
-        <img src="/img/comp_screen_1_mob_light.svg" alt="" className="w-screen mt-2 h-64 overflow-hidden tablet:hidden transform translate-x-20 translate-y-5 scale-125" />
+        <img src="/img/comp_screen_1_mob_light.svg" alt="" className="w-screen mt-2 h-64 overflow-hidden tablet:hidden transform translate-x-20 translate-y-5 scale-150" />
 
-        <div className="flex flex-col w-12/12 laptop:max-w-md mt-12 laptop:mt-20 pb-20 tablet:pb-0">
-          <Button className="bg-secondary dark:bg-darkbutton text-sm w-full tablet:w-72 laptop:text-[28px] font-medium text-white m-auto tablet:ml-0 laptop:m-auto laptop:w-421 h-12 laptop:h-24 rounded-lg laptop:rounded-3xl hover:bg-btnHover dark:hover:bg-darkBtnHover dark:text-darkButtonText ">Быстрая игра</Button>
+        <div className="flex flex-col w-12/12 laptop:max-w-md mt-12 tablet:mt-16 laptop:mt-20 pb-20 tablet:pb-0">
+          <Button className="bg-secondary dark:bg-darkbutton text-sm w-full tablet:w-80 tablet:text-[20px] laptop:text-[28px] font-medium text-white m-auto tablet:ml-0 laptop:m-auto laptop:w-421 h-12 tablet:h-14 laptop:h-24 rounded-xl tablet:rounded-2xl laptop:rounded-3xl hover:bg-btnHover dark:hover:bg-darkBtnHover dark:text-darkButtonText ">Быстрая игра</Button>
 
-          <div className="flex flex-row w-full tablet:w-72 justify-between gap-0 laptop:gap-2 mt-6 tablet:mt-5 laptop:mt-9">
+          <div className="flex flex-row w-full tablet:w-80 pt-0 justify-between gap-0 laptop:gap-2 mt-6 tablet:mt-4 laptop:mt-9 p-1">
             <Dialog className='p-[24px] w-80 '>
               <DialogTrigger>
-                <Button className="text-main tracking-wide dark:text-darkThemeText text-sm pl-0 laptop:ml-4 laptop:text-2xl hover:underline laptop:hover:text-decoration-solid">Зарегистрироваться</Button>
+                <Button className="text-main tracking-wide dark:text-darkThemeText text-sm pl-0 laptop:ml-4 tablet:text-base laptop:text-2xl hover:underline laptop:hover:text-decoration-solid">Зарегистрироваться</Button>
               </DialogTrigger>
               <DialogContent>
                 <DialogHeader>
@@ -87,7 +87,7 @@ const Hero = () => {
             
             <Dialog className='p-[24px] w-80 '>
               <DialogTrigger>
-                <Button className="text-main tracking-wide dark:text-darkThemeText text-sm laptop:text-2xl pr-0 hover:underline relative hover:text-decoration-solid">Войти</Button>
+                <Button className="text-main tracking-wide dark:text-darkThemeText text-sm tablet:text-base laptop:text-2xl pr-0 hover:underline relative hover:text-decoration-solid">Войти</Button>
               </DialogTrigger>
               <DialogContent>
                 <DialogHeader>
